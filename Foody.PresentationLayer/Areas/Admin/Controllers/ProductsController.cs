39758d49ce062554428690b14c25c158ee0c1ffd
DEV <DEV@DESKTOP-8VP4IIT>
2024-08-15 16:11:03 +0300

    #24 Foody Asp.Net Core 6.0 Project - Delete Product And Create Product View

--- Foody.PresentationLayer/Areas/Admin/Controllers/ProductsController.cs
+++ Foody.PresentationLayer/Areas/Admin/Controllers/ProductsController.cs
@@ -18,11 +18,26 @@ namespace Foody.PresentationLayer.Areas.Admin.Controllers
             var value = _productService.TGetAll();
             return View(value);
         }
-
         public IActionResult ProductListWithCategory()
         {
             var values= _productService.TProductListWithCategory();
             return View(values);
+        }
+
+        public IActionResult DeleteProduct(int id)
+        {
+            _productService.TDelete(id);
+            return RedirectToAction("ProductListWithCategory");
+        }
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateProduct(int id)
+        {
+            return View();
         }
     }
 }
